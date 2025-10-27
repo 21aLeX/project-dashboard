0fee913760d79eacc04ed75afac2d0f1221fba8e
alex <aleksandra12mail@mail.ru>
2025-10-27 20:20:25 +0800

    feat: add app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: `
+    <main>
+      <router-outlet />
+    </main>
+  `
 })
-export class App {
-  protected readonly title = signal('project-dashboard');
+export class AppComponent {
+  title = 'Project Dashboard';
 }
